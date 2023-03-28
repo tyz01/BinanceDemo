db68e8c0f2393d7d03b379756c818312b966e036
tyz01 <jenaynogach1@mail.ru>
2023-03-28 23:36:34 +0300

    delete comment

--- src/main/java/com/example/binance/config/BinanceConfig.java
+++ src/main/java/com/example/binance/config/BinanceConfig.java
@@ -1,17 +1,11 @@
 package com.example.binance.config;
 
-import com.example.binance.client.BinanceFeignClient;
-import com.example.binance.service.BinanceServiceImpl;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
-//@ConfigurationProperties(prefix = "binance")
 @Getter
 @Setter
 public class BinanceConfig {
@@ -22,11 +16,5 @@ public class BinanceConfig {
     private String secretKey;
     @Value("${binance.apiBaseUrl}")
     private String apiBaseUrl;
-
-//    @Bean
-//    public RestTemplate configRestTemplate(){
-//        return new RestTemplate();
-//    }
-
 
 }
